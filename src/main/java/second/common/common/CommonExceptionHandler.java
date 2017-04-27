6bf6b6d2d5d1a9e00b749588a7b42bd6060d7589
pch8388 <pch838811@gmail.com>
2017-04-27 15:27:05 +0900

    common exception handler    test

---
+++ src/main/java/second/common/common/CommonExceptionHandler.java
@@ -0,0 +1,13 @@
+package second.common.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CommonExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public String handleException(){
+		return "/common/error_common";
+	}
+}
